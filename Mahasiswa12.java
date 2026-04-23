b99925cbdff31decd93e459fb1fcefc4f7b6a16e
Moch.Agung Bima Ardian <bimaagungbimzz@gmail.com>
2026-04-24 04:15:14 +0700

    1

---
+++ Mahasiswa12.java
@@ -0,0 +1,19 @@
+public class Mahasiswa12 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    Mahasiswa12(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+
+}
